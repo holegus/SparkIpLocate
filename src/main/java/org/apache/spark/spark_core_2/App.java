92ee093ca79c3fece2656c34ea3788b3a811f636
Oleg Hmelnits <holegus@gmail.com>
2016-07-07 11:53:24 +0300

    Complete to produce IP number

--- src/main/java/org/apache/spark/spark_core_2/App.java
+++ src/main/java/org/apache/spark/spark_core_2/App.java
@@ -6,18 +6,24 @@ import org.apache.spark.api.java.function.Function;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 public class App {
 	  public static void main(String[] args) {
+		
+		//Regex to catch IPv6 - the string will include at least one alpha character.
+		Pattern p = Pattern.compile("[a-zA-Z]");
 		  
 	    String locationFile = "/home/tom/IP2LOCATION-LITE-DB3.CSV";
 	    //String logFile = "/home/tom/rddtestweb.log";
 	    String logFile = "hdfs://cdh-nn2/user/tom/weblog/*"; 
 	    //Should be some file on your system
 	    
-	    SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("local");
+	    SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("yarn-client");
 	    JavaSparkContext sc = new JavaSparkContext(conf);
 	    
 	    Logger rootLogger = Logger.getRootLogger();
@@ -53,7 +59,20 @@ public class App {
 	    			try {
 	    				summary = 6777216 * (long)Integer.parseInt(temp.get(0)) + 65536 * (long)Integer.parseInt(temp.get(1)) + 256 * (long)Integer.parseInt(temp.get(2)) + (long)Integer.parseInt(temp.get(3));
 	    			} catch (NumberFormatException e) {
-	    				summary = 1968L;
+	    				//  ip.split for IPv6 format will bring temp array with the single member - no "."
+	    				// So temp.size check is necessary  
+	    				if (temp.size() > 1) {
+	    					// Double check if it is IPv6 with letters.
+		    				Matcher m = p.matcher(temp.get(3));
+	    					if (temp.get(3).contains(":") && !m.find()) {
+	    						int index = temp.get(3).indexOf(":");	
+	    						// Reformat the IP last octet - trim port number and recalculate IP number.
+	    						String trimmed = temp.get(3).substring(0, index);
+	    						summary = 6777216 * (long)Integer.parseInt(temp.get(0)) + 65536 * (long)Integer.parseInt(temp.get(1)) + 256 * (long)Integer.parseInt(temp.get(2)) + (long)Integer.parseInt(trimmed);
+	    					} else {
+	    						summary = 0L;
+	    					}
+	    				}
 	    			}	
 	    			ipNumbers.add(summary);
 	    		}
@@ -62,7 +81,7 @@ public class App {
 	    }); 
 	   
 	    //System.out.println(logData.collect());
-	    System.out.println(ipSplit.collect()); 
+	    ipSplit.saveAsTextFile("/home/tom/Documents/IPstring.txt"); 
 	    sc.stop();
 	  }
 }
